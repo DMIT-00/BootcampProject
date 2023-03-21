8ea8565bc2934921c20a154c1837bcab65d8b177
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-03-21 16:44:11 +0300

    Added entities

--- repository/src/main/java/com/dmit/entity/User.java
+++ repository/src/main/java/com/dmit/entity/User.java
@@ -1,4 +1,45 @@
 package com.dmit.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "t_user")
 public class User {
+    @Id
+    @Column(name = "user_id", columnDefinition = "CHAR(36)")
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    @Column(name = "user_email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @JoinTable(name = "t_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
