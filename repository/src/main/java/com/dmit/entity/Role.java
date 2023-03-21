98dc7c65da67e5e994042a44024055a0c38fd40e
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-03-21 16:33:43 +0300

    Initial commit

---
+++ repository/src/main/java/com/dmit/entity/Role.java
@@ -0,0 +1,51 @@
+package com.dmit.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "t_role")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(
+            name = "increment",
+            strategy = "org.hibernate.id.IncrementGenerator"
+    )
+    private Long id;
+
+    @NaturalId
+    @Column(name = "role_name")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    Set<User> users = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Role role = (Role) o;
+        return roleName != null && Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+}
---
+++ repository/src/main/java/com/dmit/entity/User.java
@@ -0,0 +1,4 @@
+package com.dmit.entity;
+
+public class User {
+}
