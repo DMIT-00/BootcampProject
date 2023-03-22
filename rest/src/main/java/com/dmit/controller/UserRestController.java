7551ce5e3ad182a11bf09dcff719a9f7bde43039
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-03-22 08:39:40 +0300

    Removed test code

--- rest/src/main/java/com/dmit/controller/UserRestController.java
+++ rest/src/main/java/com/dmit/controller/UserRestController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -27,23 +24,6 @@ public class UserRestController {
         List<UserResponseDto> users = userService.findAllUsersPageable(page, size);
         if (users.isEmpty())
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
-
-    @GetMapping("users-test")
-    public ResponseEntity<List<UserResponseDto>> getTestUser(@RequestParam(value = "page", defaultValue = "0") int page,
-                                                          @RequestParam(value = "size", defaultValue = "10")
-                                                          int size) {
-        List<UserResponseDto> users = new ArrayList<>();
-        users.add(new UserResponseDto(
-                UUID.randomUUID(),
-                "aaa@gmail.com",
-                "Adam",
-                "Doe",
-                "Rex",
-                new HashSet<>())
-        );
 
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
