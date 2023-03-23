dd60a62ba07e91af2d63ef13f7383bdb6a96dfa1
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-03-23 18:22:15 +0300

    Added service documentation

--- service/src/main/java/com/dmit/service/UserService.java
+++ service/src/main/java/com/dmit/service/UserService.java
@@ -5,7 +5,25 @@ import com.dmit.dto.UserResponseDto;
 
 import java.util.List;
 
+/**
+ * The UserService interface provides methods for managing user data.
+ */
 public interface UserService {
+    /**
+     * Adds a new user to the system.
+     *
+     * @param userRequestDto The UserRequestDto object containing the user's data.
+     * @return A UserResponseDto object representing the added user.
+     */
     UserResponseDto addUser(UserRequestDto userRequestDto);
+
+    /**
+     * Returns a page of users sorted by the given criteria.
+     *
+     * @param page The page number (0-indexed) of the results to return.
+     * @param size The number of results per page.
+     * @param sort The field to sort by.
+     * @return A List of UserResponseDto objects representing the users on the requested page.
+     */
     List<UserResponseDto> findAllUsersPageableSortedBy(int page, int size, String sort);
 }
